3cceedd171e2b2edfbe714a91efce6efbe38ccbf
Parsa Nazer <parsa.nazerrr@gmail.com>
2024-01-14 20:23:52 +0330

    removing and fixing phone header still need skill to be fixed

--- personal_website5/urls.py
+++ personal_website5/urls.py
@@ -7,4 +7,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
