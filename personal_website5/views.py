c723e4c295a483b23f637be1650955cf63b078dc
Parsa Nazer <parsa.nazerrr@gmail.com>
2024-01-16 23:55:01 +0330

    testing bot

--- personal_website5/views.py
+++ personal_website5/views.py
@@ -5,6 +5,9 @@ from aboutme.models import AboutMe, Info
 from skills.models import Skill, Skill_text
 from comment.models import Comment
 from certificate.models import Certificate_text, Certificate
+import telebot
+TOKEN = "6700654306:AAElY-6rjRUJ9WUWgH0esyutkiod_0YcUUY"
+CHAT_ID = 1198382521
 
 def index(request):
     context = {
@@ -21,6 +24,8 @@ def index(request):
     if request.method == 'POST':
         comment = Comment(name=request.POST.get('name'), email=request.POST.get('email'), comment=request.POST.get('comment'))
         comment.save()
+        bot = telebot.TeleBot(TOKEN)
+        bot.send_message(CHAT_ID, f"name: {request.POST.get('name')}\nemail: {request.POST.get('email')}\ncomment: {request.POST.get('comment')}")
         return redirect('/')
     return render(request, 'index/index-rtl.html', context)
 
